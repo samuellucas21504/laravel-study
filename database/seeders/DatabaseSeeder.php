f3dd268769a661f3ff9194164be61adaa584b231
Samuel Lucas <samuellucasrdg@gmail.com>
2023-05-09 17:50:06 -0300

    Created Addresses table, factory, model and seeder for future tests.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Post, Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Address;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Post::factory(200)->create();
+        User::factory(100)->create();
+        Address::factory(100)->create();
     }
 }
